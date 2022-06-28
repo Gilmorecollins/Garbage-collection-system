3d9acbb585c9b0c1cdf855e8056c02e26ae6601a
Gilmore Collins <46931312+Gilmorecollins@users.noreply.github.com>
2022-06-28 10:20:12 +0300

    Add files via upload

---
+++ system/includes/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<!-- Created By CodingLab - www.codinglabweb.com -->
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="UTF-8">
+    <title> Authentication </title>
+    <link rel="stylesheet" href="assets/style.css">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   </head>
+<body>
+
+</body>
+
+</html>
